2385d34c62a7962474bf505cc8fcc42dd7ff952c
Byun Kyung MIn <chgemn@gmail.com>
2023-04-23 14:37:06 +0900

    Update theme color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
+    const [visible, setVisible] = useState(false);
+    const handleMouseOver = () => {
+        setVisible(true);
+    };
+
+    const handleMouseOut = () => {
+        setVisible(false);
+    };
+
     return (
         <NavBarWrapper>
             <TitleText>r-kyv</TitleText>
             <div>
-                <Link to="/works">works</Link>
+                <Link to="/works" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>works</Link>
                 <Link to="/info">info</Link>
             </div>
+            {!visible && <HoverEffectWrapper>WORKS</HoverEffectWrapper>}
         </NavBarWrapper>
     )
 }
@@ -34,4 +45,15 @@ const Link = styled(NavLink)`
 const TitleText = styled.h1`
     margin: 0;
     font-family: "NanumSquareNeo-H";
+`
+
+const HoverEffectWrapper = styled.h1`
+    height: 100vh;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-family: "NanumSquareNeo-H";
+    font-size: 20vw;
+    color: rgba(0,0,255,0.8)
 `
